172011c8b2fc05c111cfe116f3bced91428d050f
Lukáš Huvar <137092577+LukasRaider@users.noreply.github.com>
2023-11-25 01:06:17 +0100

    Add files via upload

---
+++ SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+namespace SpecFLowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Add() { return FirstNumber + SecondNumber; }
+    }
+}
