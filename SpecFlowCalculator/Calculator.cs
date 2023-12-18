73a258786a79f043071b6d26d58ba200d21ed3ad
Lukáš Huvar <137092577+LukasRaider@users.noreply.github.com>
2023-12-18 01:05:08 +0100

    Add files via upload

--- SpecFlowCalculator/Calculator.cs
+++ SpecFlowCalculator/Calculator.cs
@@ -4,10 +4,15 @@ namespace SpecFLowCalculator
 {
     public class Calculator
     {
-        public int FirstNumber { get; set; }
-        public int SecondNumber { get; set; }
-        public int Add() { return FirstNumber + SecondNumber; }
+        public int FirstNumber { get; set; } // property prvniho cisla
+        public int SecondNumber { get; set; } //proprety dru cislo
+        public int Add() { return FirstNumber + SecondNumber; } //scitani dvou cisel
+        public int Subtract() { return FirstNumber - SecondNumber; } //rozdil dvou cisel
+        public int Multiplication() { return FirstNumber * SecondNumber; } //nasobeni dvou cisel
+        public float Division() {  return FirstNumber / SecondNumber; } //deleni dvou cisel
+        public int Residue() { return FirstNumber % SecondNumber; } //deleni zbytek dvou cisel
+        public int EmpowermentFirstNumber() { return FirstNumber * FirstNumber; } //umocnovani normalni cislo
+		public int EmpowermentSecondNumber() { return SecondNumber * SecondNumber; } //umocnovani zaporne cislo
 
-        public int Subtract(){return FirstNumber + SecondNumber;}
-    }
+	}
 }
